64fd4dd2d623ac29f91fda3608e6c4fbd06b0f4a
Samuel Florez Villaquiran <1110366605@u.icesi.edu.co>
2025-03-17 09:41:55 -0500

    se agregaron componentes extras de js

--- script.js
+++ script.js
@@ -2,6 +2,140 @@
 
 
 
+
+let numero = prompt("Ingresa un numero:");
+if (numero >= 0){
+
+    alert("El numero es positivo")
+
+} else {
+
+    alert("El numero es negativo")
+
+}
+console.log("Siguiente flujo");
+
+
+
+
+
+
+//Operadores de comparacion 
+/*
+let a = 10;
+
+console.log( a == 5);
+console.log( a != 5);
+console.log( a > 5);
+console.log( a >= 5);
+console.log( a < 5);
+console.log( a==10? 'es diez':'no es diez');
+
+console.log(a === '10', "Compara cadena");
+console.log(a !== '10', "Compara cadena");
+*7
+
+
+
+
+
+/*
+let a = 10;
+let b = a + 5;
+
+a += 5;
+a -= 5;
+a *= 5;
+a /= 5;
+a %= 5;
+a **= 5;
+
+
+console.log(a);
+*7
+
+/*
+let x ='10';
+let y ='20';
+
+console.log(x + y);
+*/
+
+
+
+//operadores aritmeticos
+
+
+/*
+let a = 20;
+let b = 5;
+let suma = a + b;
+console.log(suma);
+*/
+
+/*
+console.log(a + b); //suma 25
+console.log(a - b); //resta 15
+console.log(a * b); //multiplicacion 100
+console.log(a / b); //division 4
+console.log(a % b); //modulo 0
+console.log(a ** b); //potencia 3.200.000
+console.log(++a); //Pre-icremento 21
+console.log(a++); //Post-Incremento 21
+console.log(a);
+console.log(--a); //decrementar
+console.log(a--); //decrementar
+
+let contador = 1;
+contador++;
+
+contador = contador + 1;
+
+console.log(contador);
+
+*/
+
+
+/*
+let number = 15 * "hola";
+
+if (isNaN(number)){
+
+    console.log("sos pendejo");
+}
+
+console.log(number)
+
+*/
+
+
+
+
+/*
+//tipo sde datos mas basicos en JS
+
+let numero = 10;  //number
+let texto = "hola mundo";  //string
+let verdadero = true;  //boolean
+let falso = false;  //boolean
+let nodefinido;  //undefined
+let indefinido;  //undefined
+indefinido;
+let nulo = null;  //Null
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof verdadero);
+console.log(typeof indefinido);
+console.log(nulo);
+
+*/
+
+
+
+
+
+/*
 const PI = 3.1416  //esto es una constante (significa que su valor no va a cambiar por nada)
 PI = 45            //aqui se intenta cambiar, pero si se hace EventSource, ocurre error
 
@@ -11,7 +145,7 @@ console.log(PI);
 let numero = 100   //declarando la variable (a cuanto equivale)
 
 console.log(numero);   //se extrae esa vaiable
-
+*/
 
 /*
 Colocar nombre en la consola
