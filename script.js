6201607ddb72e27993e120defc28aabe6bd636f5
Samuel Florez Villaquiran <1110366605@u.icesi.edu.co>
2025-03-17 08:22:08 -0500

    que es const?

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 
+
+
+
+const PI = 3.1416  //esto es una constante (significa que su valor no va a cambiar por nada)
+PI = 45            //aqui se intenta cambiar, pero si se hace EventSource, ocurre error
+
+console.log(PI);    
+
+
+let numero = 100   //declarando la variable (a cuanto equivale)
+
+console.log(numero);   //se extrae esa vaiable
+
+
+/*
+Colocar nombre en la consola
+
 let nombre = prompt("Ingresa tu Nombre");
 console.log('Hola ' + nombre);
 
-/*Colocar nombre en la consola*/
+*/
